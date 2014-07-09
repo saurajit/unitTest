6ec87aef26083e0d1d5a73ed76bf7bbd659ed334
Saura <saurajitb@gmail.com>
2014-07-09 12:28:52 +0530

    Modified service call implementations

--- app/scripts/controllers/geoNameCtrl.js
+++ app/scripts/controllers/geoNameCtrl.js
@@ -15,8 +15,40 @@ angular.module('unittestApp')
       'Karma'
     ];
 
+    $scope.selectedCountry = undefined;
+    $scope.allCountries = [];
+    $scope.earthquakeInfoList = [];
+
     $scope.searchGeoname = function () {
-      geoNameService.countryInfo();
+      $scope.allCountries = [];
+      angular.element('select option[value=""]').html("Loading...");
+      geoNameService.countryInfo()
+        .success(function (response, status) {
+          if(status === 200) {
+            $scope.allCountries = response.geonames;
+            angular.element('select option[value=""]').html("Select a country");
+          }
+        })
+        .error(function () {
+          $scope.allCountries = [];
+          angular.element('select option[value=""]').html("Error loading country list..");
+        });
+    };
+
+    $scope.countryChanged = function () {
+      $scope.earthquakeInfoList = [];
+      if ($scope.selectedCountry) {
+        var country = $scope.selectedCountry;
+        geoNameService.earthquakeInfo(country.north, country.south, country.east, country.west)
+          .success(function (response, status){
+            if (status === 200) {
+              $scope.earthquakeInfoList = response.earthquakes;
+            }
+          })
+          .error(function (){
+            $scope.earthquakeInfoList = [];
+          });
+      }
     };
 
     $scope.searchGeoname();
--- app/scripts/services/geoName.js
+++ app/scripts/services/geoName.js
@@ -10,21 +10,53 @@
 angular.module('unittestApp')
   .factory('geoNameService', ['$http', function ($http) {
 
-    var _API_USERNAME = 'gumnam';
+    var _API_USERNAME = 'demo'; //Use the 'username' by creating an account at http://www.geonames.org/login
 
-    var countryInfo = 'http://api.geonames.org/countryInfoJSON';
+    var countryInfo = 'http://api.geonames.org/countryInfoJSON',
+      earthquakes = 'http://api.geonames.org/earthquakesJSON';
+    var defaultParams  = {
+      'username': _API_USERNAME
+    };
 
     // Public API here
     return {
+      /**
+       * @ngdoc function
+       * @name unittestApp.geonameSearch.countryInfo
+       * @description
+       * # countryInfo
+       * Get all the country list from Geoname.
+       */
       countryInfo: function (searchTerm, style, numRows) {
         var config = {
           'params': {
-            'username': _API_USERNAME,
             'style': (style || 'SMALL'),
             'maxRows': (numRows || null)
           }
         };
+        angular.extend(config.params, defaultParams);
         return $http.get(countryInfo, config);
+      },
+      /**
+       * @ngdoc function
+       * @name unittestApp.geonameSearch.earthquakeInfo
+       * @description
+       * # earthquakeInfo
+       * Get earthquake information for a country from Geoname.
+       */
+      earthquakeInfo: function (north, south, east, west, style, numRows) {
+        var config = {
+          'params': {
+            'style': (style || 'FULL'),
+            'maxRows': (numRows || null),
+            'north': north,
+            'south': south,
+            'east': east,
+            'west': west
+          }
+        };
+        angular.extend(config.params, defaultParams);
+        return $http.get(earthquakes, config);
       }
     };
   }]);
